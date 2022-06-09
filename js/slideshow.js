e681c1d5778e46d9f4fd67b4d7c069e2f414dbdc
zakruvalcaba <zakruvalcaba@gmail.com>
2022-06-09 15:00:20 -0700

    Updated code

--- js/slideshow.js
+++ js/slideshow.js
@@ -1,90 +1,91 @@
-const $ = (id) => {
-    return document.getElementById(id);
-}
+// HELPER FUNCTION TO GET DOM ELEMENTS
+const $ = (id) => document.getElementById(id)
 
+// SLIDESHOW OBJECT
 const slideshow = function () {
     // PRIVATE MEMBERS
-    let timer;
-    let play = true;
-    let nodes = {image: null, caption: null};
-    let img = {cache: [], counter: 0};
-
+    let timer
+    let play = true
+    let nodes = {image: null, caption: null}
+    let img = {cache: [], counter: 0}
+    // PRIVATE: STOPS SLIDESHOW
     const stopSlideShow = function () {
-        clearInterval(timer);
+        clearInterval(timer)
     }
-
+    // PRIVATE: DISPLAYS THE NEXT IMAGE
     const displayNextImage = function () {
         if (img.counter === img.cache.length - 1) {
-            img.counter = 0;
+            img.counter = 0
         } else {
-            img.counter++;
+            img.counter++
         }
-        let image = img.cache[img.counter];
-        nodes.image.src = image.src;
-        nodes.caption.innerHTML = image.title;
+        let image = img.cache[img.counter]
+        nodes.image.src = image.src
+        nodes.caption.innerHTML = image.title
     }
-
+    // PRIVATE: DISPLAYS PLAY/PAUSE TEXT ON BUTTON
     const setPlayText = function (btn) {
         if (play) {
-            btn.value = 'Resume';
+            btn.value = 'Resume'
         } else {
-            btn.value = 'Pause';
+            btn.value = 'Pause'
         }
     }
 
     // PUBLIC MEMBERS
     return {
         loadImages: function (slides) {
-            let image;
+            let image
             for (let i = 0; i < slides.length; i++) {
-                image = new Image();
-                image.src = slides[i].href;
-                image.title = slides[i].title;
-                img.cache.push(image);
+                image = new Image()
+                image.src = slides[i].href
+                image.title = slides[i].title
+                img.cache.push(image)
             }
-            return this;
+            return this
         },
         startSlideShow: function () {
             if (arguments.length === 2) {
-                nodes.image = arguments[0];
-                nodes.caption = arguments[1];
+                nodes.image = arguments[0]
+                nodes.caption = arguments[1]
             }
-            timer = setInterval(displayNextImage, 2000);
-            return this;
+            timer = setInterval(displayNextImage, 2000)
+            return this
         },
-        createToggleHandler: function () { 
-            let me = this;
+        createToggleHandler: function () {
+            let me = this
             // CLOSURE USED AS THE CLICK EVENT HANDLER
             return function () {
-                // 'THIS' IS THE CLICKED BUTTON 
-                // 'ME' IS THE OBJECT LITERAL
+                // 'this' REPRESENTS THE CLICKED BUTTON
+                // 'me' IS THE OBJECT LITERAL
                 if (play) {
-                    stopSlideShow();
+                    stopSlideShow()
                 } else {
-                    me.startSlideShow();
+                    me.startSlideShow()
                 }
-                setPlayText(this);
+                setPlayText(this)
                 // TOGGLE BETWEEN TRUE AND FALSE
-                play = !play;
-            };
+                play = !play
+            }
         }
-    };
-};
+    }
+}
 
-// CREATE THE SLIDESHOW OBJECT
-const newSlideShow = slideshow();
+// CRRATE NEW SLIDESHOW OBJECT
+const newSlideShow = slideshow()
 
-window.addEventListener('load', () => {
-    const slides = [
-        {href: 'images/backpack.jpg', title: 'He loves backpacking in the Sierras'},
-        {href: 'images/boat.jpg', title: 'He loves his boat'},
-        {href: 'images/camaro.jpg', title: 'He loves his Camaro more'},
-        {href: 'images/punk.jpg', title: 'He used to be in a punk band and toured with numerous bands'},
-        {href: 'images/race.jpg', title: 'He loves obstacle course racing'},
-        {href: 'images/wakeboard.jpg', title: 'He loves wakeboarding and wakesurfing'}
-    ];
-    // START SLIDESHOW
-    newSlideShow.loadImages(slides).startSlideShow($('image'), $('caption'));
-    // ATTACH EVENT HANDLER TO PLAY/PAUSE BUTTON
-    $('play_pause').onclick = newSlideShow.createToggleHandler();
-});
+// CREATE ARRAY OF SLIDES
+const slides = [
+    {href: 'images/backpack.jpg', title: 'Zak used to love to backpack'},
+    {href: 'images/boat.jpg', title: 'Zak loves his boat'},
+    {href: 'images/camaro.jpg', title: 'Zak sold his Camaro'},
+    {href: 'images/punk.jpg', title: 'Zak used to be in a punk band'},
+    {href: 'images/race.jpg', title: 'Zak loves to obstacle course race'},
+    {href: 'images/wakeboard.jpg', title: 'Zak loves to wakeboard'}
+]
+
+// START SLIDESHOW
+newSlideShow.loadImages(slides).startSlideShow($('image'), $('caption'))
+
+// ATTACH EVENT HANDLER TO PLAY/PAUSE BUTTON
+$('play_pause').onclick = newSlideShow.createToggleHandler()
